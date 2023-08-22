61029e6c7353916bdce14ac088a8ae459a130444
Max Zabelenkov <maxx.zabelenkov@gmail.com>
2023-08-22 15:41:12 +0300

    add slideer testimonials and contact (30%)

---
+++ js/swiper.js
@@ -0,0 +1,12 @@
+new Swiper('.testimonials__swiper', {
+    slidesPerView: 1,
+    loop: true,
+
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+    }
+
+},);
